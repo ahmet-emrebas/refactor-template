e94fa36c8ae62a0f07b970bea0e06205cb33d946
ahmet-emrebas <none>
2021-03-02 18:31:20 -0600

    tested and documented isRelativePathValid, resolveRelativePath, and parserWorkingRelativePath methods

--- src/FileTreeRefactor.ts
+++ src/FileTreeRefactor.ts
@@ -2,46 +2,98 @@ import { join } from 'path';
 
 // Exceptions
 
+/** Thrown when path is NOT valid. */
 export class PathIsNotValidException extends Error {
   constructor() {
-    super('Path is empty or contains forbidden characters, :, ?, >, <, or "');
+    super('Path is empty or contains forbidden characters, :, ?, >, <, or "!!!');
   }
-  static throw() {
-    throw new PathIsNotValidException();
+  static toString() {
+    return PathIsNotValidException.name;
   }
 }
 
-// Class
+/** Thrown when the path is empty. */
+export class PathIsEmptyException extends Error {
+  constructor() {
+    super('Path is empty!!!');
+  }
+  static toString() {
+    return PathIsEmptyException.name;
+  }
+}
 
 export class FileTreeRefactor {
-  private workingRelativePath: string;
+  /**
+   * Directory to be process.
+   * @example users, products, products-one etc.
+   * @type string
+   */
+  private readonly workingRelativePath: string;
 
   /**
-   * @param directoryRelativePath Ex. src/users, src/services/product-service
-   * @param workingRelativePath Ex users, product-service
+   * Relative path to the directory.
+   * @example src/users, src/products, src/products/products-one
+   * @type string
    */
-  constructor(private directoryRelativePath: string) {}
+  private readonly directoryRelativePath: string;
 
   /**
-   * Resolve backslash problem.
-   * @example input: src/users, output: src\\users.
-   * @param fileRelativePath Ex. src/users, src/service
+   * List of FileTreeRefactor, representing folders and files.
+   * @types FileTreeRefactor[]
    */
-  static resolveRelativePath(fileRelativePath: string) {
-    this.isRelativePathValid(fileRelativePath);
-    return join('', fileRelativePath);
+  private branches: FileTreeRefactor[] = [];
+
+  /**
+   * @param {string} directoryRelativePath Relative path to the directory. Ex. src/users, src/products, src/products/products-one.
+   */
+  constructor(directoryRelativePath: string) {
+    this.directoryRelativePath = directoryRelativePath;
+  }
+
+  /**
+   * Refactor the current directory three to new one.
+   * To generate a new folder from the configuration of this instance with a different directory path newDirectoryRelativePath.
+   * @param {string} newDirectoryRelativePath
+   * @returns void
+   * @throws PathIsNotValidException, PathIsEmptyException.
+   */
+  public refactorTo(newDirectoryRelativePath: string) {
+    FileTreeRefactor.isRelativePathValid(newDirectoryRelativePath);
+    throw new Error('Not Implmented!');
+  }
+
+  /**
+   * Parse the workingRelative path from the relative path.
+   * @example parserWorkingRelativePath('src/users') will return 'users'.
+   * @param relativePath
+   * @returns string
+   * @throws PathIsNotValidException, PathIsEmptyException.
+   */
+  static parseWorkingRelativePath(relativePath: string) {
+    return this.resolveRelativePath(relativePath).split('\\').pop();
+  }
+
+  /**
+   * Resolve relative path as nodejs path.
+   * @param relativePath
+   * @example resolveRelativePat('src/users') return 'src\users'.
+   * @returns string
+   * @throws PathIsNotValidException, PathIsEmptyException.
+   */
+  static resolveRelativePath(relativePath: string) {
+    this.isRelativePathValid(relativePath);
+    return join('', relativePath);
   }
 
   /**
    * Relative path must NOT be empty and Not contains any item in the list [ '?', '<', '>', '"' , ":" "]
    * @param path {string} relativePath
    * @returns true
-   * @throws PathIsNotValidException
+   * @throws PathIsNotValidException, PathIsEmptyException
    */
   static isRelativePathValid(relativePath: string) {
-    if (relativePath == '' || relativePath.match(new RegExp(/[?|<|>|"|:]/))) {
-      throw new PathIsNotValidException();
-    }
+    if (relativePath === '' || !relativePath) throw new PathIsEmptyException();
+    if (relativePath.match(new RegExp(/[?|<|>|"|:]/))) throw new PathIsNotValidException();
     return true;
   }
 }
